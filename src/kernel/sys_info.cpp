50a6b0cdad41d1b91f0d6051c45d5bcdbcbc4ca1
My-Bad-2 <yashv7883@gmail.com>
2023-07-04 08:32:08 -0400

    Minor additions

--- src/kernel/sys_info.cpp
+++ src/kernel/sys_info.cpp
@@ -4,7 +4,7 @@
 #include <sys_info.hpp>
 #include <time/time.hpp>
 #include <arch.hpp>
-#include "limine.h"
+#include <memory/physical.hpp>
 
 namespace kernel {
 // static uint64_t seconds_since_boot() {
@@ -12,10 +12,13 @@ namespace kernel {
 // }
 
 void get_sys_info() {
+    uint64_t total_memory = (memory::physical::total()) / 1000000;
+
     log::info << "System name:    \t" << PROJ_NAME << '\n';
     log::info << "System version: \t" << PROJ_VERSION << '\n';
     log::info << "Build time:     \t" << __TIMESTAMP__ << '\n';
-    log::info << "5lvl Paging:    \t" << ((paging_mode_request.response->mode == LIMINE_PAGING_MODE_MAX) ? "enabled" : "disabled") << '\n';
+    log::info << "5lvl paging:    \t" << ((paging_mode_request.response->mode == LIMINE_PAGING_MODE_MAX) ? "enabled" : "disabled") << '\n';
+    log::info << "Total memory:   \t" << total_memory << "MB\n";
 
     // log::info << "Time since boot:\t" << Hex64(seconds_since_boot()) << '\n';
     // Issue while printing time.
